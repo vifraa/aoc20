21b9ee4137017cd4ad363121a031632068e713e3
Viktor Franzen <viktor_franzen@hotmail.com>
2020-12-16 17:10:15 +0100

    Cleaner solution for part 1.

--- day10/main.go
+++ day10/main.go
@@ -13,38 +13,25 @@ func main() {
 	fmt.Printf("Part 2: %d\n", Part2(input))
 }
 
-
 func Part1(input []int) int {
 	start := 0
-	oneDiff := 0
-	twoDiff := 0
-	threeDiff := 0
+	diff := make(map[int]int)
 
 	sort.Ints(input)
-
 	prevNum := start
 	for _, i := range input {
 		temp := i - prevNum
 
-		if temp == 1 {
-			oneDiff++
-		} else if temp == 2 {
-			twoDiff++
-		} else if temp == 3 {
-			threeDiff++
-		} else {
-			return 0
-		}
+		diff[temp]++
 		prevNum = i
 	}
 
-	threeDiff++
+	diff[3]++
 
-	return oneDiff * threeDiff
+	return diff[1] * diff[3]
 }
 
 func Part2(input []int) int {
-
 
 	return 0
 }
