e9d9ffda43a06418984814691739012f6fbf388f
Viktor Franzen <viktor_franzen@hotmail.com>
2020-12-17 16:50:47 +0100

    Work with copy.

--- day11/main.go
+++ day11/main.go
@@ -12,11 +12,17 @@ func main() {
 	fmt.Printf("Part 2: %d\n", Part2(input))
 }
 
-
 func Part1(input []string) int {
 	counter := 0
-	for !util.EqualsStringSlice(input, modelSeatMovement(input)) {
+	for true {
+		tmp := modelSeatMovement(input)
+
+		if util.EqualsStringSlice(input, tmp) {
+			return counter
+		}
+
 		counter++
+		input = tmp
 	}
 	return counter
 }
@@ -26,18 +32,21 @@ func Part2(input []string) int {
 }
 
 func modelSeatMovement(input []string) []string {
-	for x := 0; x < len(input); x++ {
+	tmp := make([]string, len(input))
+	copy(tmp, input)
+
+	for x := 0; x < len(tmp); x++ {
 		row := input[x]
 		for y := 0; y < len(row); y++ {
 			elem := row[y]
 			switch elem {
 			case 'L':
 				if GetOccupiedAdjacentSeats(input, x, y) == 0 {
-					input[x] = input[x][0:y] + "#" + input[x][y+1:]
+					tmp[x] = input[x][0:y] + "#" + input[x][y+1:]
 				}
 			case '#':
 				if GetOccupiedAdjacentSeats(input, x, y) >= 4 {
-					input[x] = input[x][0:y] + "L" + input[x][y+1:]
+					tmp[x] = input[x][0:y] + "L" + input[x][y+1:]
 				}
 			default:
 				continue
@@ -45,15 +54,14 @@ func modelSeatMovement(input []string) []string {
 		}
 	}
 
-	return input
+	return tmp
 }
 
-
-func GetOccupiedAdjacentSeats(seats []string, row,col int) int {
+func GetOccupiedAdjacentSeats(seats []string, row, col int) int {
 	directions := []struct {
 		dx int
 		dy int
-	}{{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}}
+	}{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}
 
 	found := 0
 	for _, dir := range directions {
