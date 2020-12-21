d42ab6cac293a6d65309e83f7e1de3cd099081c2
Viktor Franzen <viktor_franzen@hotmail.com>
2020-12-21 19:08:49 +0100

    Fix test input and expected value.

--- day11/main_test.go
+++ day11/main_test.go
@@ -3,20 +3,20 @@ package main
 import "testing"
 
 var input = []string{
-	"#.##.##.##",
-	"#######.##",
-	"#.#.#..#..",
-	"####.##.##",
-	"#.##.##.##",
-	"#.#####.##",
-	"..#.#.....",
-	"##########",
-	"#.######.#",
-	"#.#####.##",
+	"L.LL.LL.LL",
+	"LLLLLLL.LL",
+	"L.L.L..L..",
+	"LLLL.LL.LL",
+	"L.LL.LL.LL",
+	"L.LLLLL.LL",
+	"..L.L.....",
+	"LLLLLLLLLL",
+	"L.LLLLLL.L",
+	"L.LLLLL.LL",
 }
 
 func TestPart1(t *testing.T) {
-	expected := 5
+	expected := 37
 	actual := Part1(input)
 	if expected != actual {
 		t.Errorf("Expected: %d, Got: %d", expected, actual)
@@ -38,4 +38,3 @@ func TestGetOccupiedAdjacentSeats(t *testing.T) {
 		t.Errorf("Expected: %d, Got: %d", expected, actual)
 	}
 }
-
